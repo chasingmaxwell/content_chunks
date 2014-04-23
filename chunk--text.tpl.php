77239f2302cc28559d0861424580e816e183c6fa
Peter Sieg <chasingmaxwell@gmail.com>
2014-04-23 14:32:48 -0400

    Simplify theme implementation for chunks.

---
+++ chunk--text.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Default chunk template.
+ */
+
+/**
+ * @TODO: add documentation about variables.
+ */
+
+?>
+
+<div class="<?php print $classes; ?>">
+  <?php print check_plain($configuration['text']); ?>
+</div>
+
