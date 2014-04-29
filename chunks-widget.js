f4030d30a653d78a00d04d16a5da38ab4cb4199f
Peter Sieg <chasingmaxwell@gmail.com>
2014-04-29 10:50:32 -0400

    Add chunk type configuration validation and simplify #states.

--- chunks-widget.js
+++ chunks-widget.js
@@ -21,24 +21,40 @@
     attach: function(context, settings) {
       // Do some stuff
       $('.chunks-field', context).each(function() {
-        var fieldName, langcode;
+        var fieldName, classFieldName, langcode;
 
-        fieldName = $(this).attr('field_name').replace(/_/g, '-');
+        fieldName = $(this).attr('field_name');
+        classFieldName = $(this).attr('field_name').replace(/_/g, '-');
         langcode = $(this).attr('langcode');
 
         $('.item', $(this)).each(function(delta, element) {
-          var classPrepend = '.' + fieldName + '-' + delta + '-';
-          $(classPrepend + 'preview-button', element).bind('mousedown.chunkPreview', function(e) {
-            $(classPrepend + 'configuration').hide();
-            $(classPrepend + 'edit-button').show();
-            $(this).hide();
-            removeActiveState(this);
+          var classPrepend, viewElement;
+
+          classPrepend = '.' + classFieldName + '-' + delta + '-';
+          viewElement = $(':input[name="' + fieldName + '[' + langcode + '][' + delta + '][view]"]');
+
+          // Switch to configuration view when errors are detected.
+          if ($(classPrepend + 'configuration .error', element).length > 0) {
+            viewElement.val('configuration');
+            viewElement.trigger('change');
+          }
+
+          // Switch to configuration view upon type selection.
+          $(':input[name="' + fieldName + '[' + langcode + '][' + delta + '][type]"]', element).bind('click.chunkTypeSelected', function(e) {
+            viewElement.val('configuration');
+            viewElement.trigger('change');
           });
+
+          // Switch to preview view when "Edit" button is selected.
+          $(classPrepend + 'preview-button', element).bind('mousedown.chunkPreview', function(e) {
+            viewElement.val('preview');
+            viewElement.trigger('change');
+          });
+
+          // Switch to configuration view when "Edit" button is selected.
           $(classPrepend + 'edit-button', element).bind('mousedown.chunkEdit', function(e) {
-            $(classPrepend + 'configuration').show();
-            $(classPrepend + 'preview-button').show();
-            $(this).hide();
-            removeActiveState(this);
+            viewElement.val('configuration');
+            viewElement.trigger('change');
           });
         });
 
