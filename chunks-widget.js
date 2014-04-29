6dbecba6abfcfdfe269a37a30f3c2692436a3325
Peter Sieg <chasingmaxwell@gmail.com>
2014-04-29 12:36:08 -0400

    Focus the "add" button when the "preview" button is pressed. Use #default_value for the view rather than #value so it can be changed with javascript client side.

--- chunks-widget.js
+++ chunks-widget.js
@@ -21,17 +21,27 @@
     attach: function(context, settings) {
       // Do some stuff
       $('.chunks-field', context).each(function() {
-        var fieldName, classFieldName, langcode;
+        var fieldName, classFieldName, langcode, chunks, setActiveChunk;
 
         fieldName = $(this).attr('field_name');
         classFieldName = $(this).attr('field_name').replace(/_/g, '-');
         langcode = $(this).attr('langcode');
+        chunks = $('.chunk-wrapper', $(this));
 
-        $('.item', $(this)).each(function(delta, element) {
-          var classPrepend, viewElement;
+        // Helper function to set "active" class on a specific chunk.
+        setActiveChunk = function(chunk) {
+          chunks.removeClass('active');
+          chunk.addClass('active');
+        };
+
+        chunks.each(function(delta, element) {
+          var classPrepend, viewElement, view, active, addButton;
 
           classPrepend = '.' + classFieldName + '-' + delta + '-';
           viewElement = $(':input[name="' + fieldName + '[' + langcode + '][' + delta + '][view]"]');
+          view = viewElement.val();
+          active = $(element).hasClass('active');
+          addButton = $(':input[name="' + fieldName + '-' + delta + '-add-after"]');
 
           // Switch to configuration view when errors are detected.
           if ($(classPrepend + 'configuration .error', element).length > 0) {
@@ -43,19 +53,31 @@
           $(':input[name="' + fieldName + '[' + langcode + '][' + delta + '][type]"]', element).bind('click.chunkTypeSelected', function(e) {
             viewElement.val('configuration');
             viewElement.trigger('change');
+            // Set active class on the last chunk with user interaction.
+            setActiveChunk($(element));
           });
 
           // Switch to preview view when "Preview" button is selected.
           $(classPrepend + 'preview-button', element).bind('mousedown.chunkPreview', function(e) {
             viewElement.val('preview');
             viewElement.trigger('change');
+            // Set active class on the last chunk with user interaction.
+            setActiveChunk($(element));
+            addButton.focus();
           });
 
           // Switch to configuration view when "Edit" button is selected.
           $(classPrepend + 'edit-button', element).bind('mousedown.chunkEdit', function(e) {
             viewElement.val('configuration');
             viewElement.trigger('change');
+            // Set active class on the last chunk with user interaction.
+            setActiveChunk($(element));
           });
+
+          // Always set focus to the active chunk's button.
+          if (active) {
+            addButton.focus();
+          }
         });
 
       });
