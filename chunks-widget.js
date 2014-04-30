9041b26c4b574237d4e5119acefd2b2dec02c4d6
Peter Sieg <chasingmaxwell@gmail.com>
2014-04-30 14:18:17 -0400

    Add Cancel button back in. Give immediate feedback when adding a chunk by loading an extra hidden chunk and displaying it instantly when button is pressed. Still need to change the submission and callback functions for those buttons so that an additional chunk is appended to the field instead of replacing the entire field.

--- chunks-widget.js
+++ chunks-widget.js
@@ -21,7 +21,7 @@
     attach: function(context, settings) {
       // Do some stuff
       $('.chunks-field', context).each(function() {
-        var fieldName, classFieldName, langcode, chunks, setActiveChunk;
+        var fieldName, classFieldName, langcode, chunks, setActiveChunk, showStagedChunk;
 
         fieldName = $(this).attr('field_name');
         classFieldName = $(this).attr('field_name').replace(/_/g, '-');
@@ -34,20 +34,34 @@
           chunk.addClass('active');
         };
 
-        chunks.each(function(delta, element) {
-          var classPrepend, viewElement, view, active, addButton;
+        // Helper function to show a staged chunk.
+        showStagedChunk = function(prevSibling) {
+          var stagedRow, stagedChunk, stagedDelta, stagedViewElement;
+          stagedRow = $('tr.staged', context);
+          stagedChunk = $('.chunk-wrapper', stagedRow);
+          stagedDelta = stagedChunk.attr('delta');
+          stagedViewElement = $(':input[name="' + fieldName + '[' + langcode + '][' + stagedDelta + '][view]"]');
+          stagedViewElement.val('type_selection');
+          stagedViewElement.trigger('change');
+          stagedRow.insertAfter(prevSibling);
+          stagedRow.show();
+          // @TODO: reset weights.
+          // @TODO: reset odd/even classes.
+        };
 
+        chunks.each(function(index, element) {
+          var delta, classPrepend, viewElement, view, active, addButton;
+
+          delta = $(element).attr('delta');
           viewElement = $(':input[name="' + fieldName + '[' + langcode + '][' + delta + '][view]"]');
           view = viewElement.val();
           classPrepend = '.' + classFieldName + '-' + delta + '-';
           active = $(element).hasClass('active');
           addButton = $(':input[name="' + fieldName + '-' + delta + '-add-after"]');
 
-          // If this chunk has been removed, do not display it and stop all
-          // other processing.
-          if (view == 'removed') {
+          // If this chunk is staged, hide it.
+          if (view == 'staged') {
             $(element).parents('#' + fieldName + '-' + delta + '-chunk-row').hide();
-            return;
           }
 
           // Switch to configuration view when errors are detected.
@@ -64,34 +78,61 @@
             setActiveChunk($(element));
           });
 
-          // Switch to preview view when "Preview" button is selected.
+          // Switch to preview view when "Preview" button is pressed.
           $(classPrepend + 'preview-button', element).bind('mousedown.chunkPreview', function(e) {
             viewElement.val('preview');
             viewElement.trigger('change');
             // Set active class on the last chunk with user interaction.
             setActiveChunk($(element));
             addButton.focus();
+            // Remove active state on button.
+            removeActiveState(this);
           });
 
-          // Switch to configuration view when "Edit" button is selected.
+          // Switch to configuration view when "Edit" button is pressed.
           $(classPrepend + 'edit-button', element).bind('mousedown.chunkEdit', function(e) {
             viewElement.val('configuration');
             viewElement.trigger('change');
             // Set active class on the last chunk with user interaction.
             setActiveChunk($(element));
+            // Remove active state on button.
+            removeActiveState(this);
           });
 
-          // Switch to removed view when "Remove" button is selected.
+          // Swith to preview view with "Cancel" button is pressed.
+          $(classPrepend + 'cancel-button', element).bind('mousedown.chunkEditCancel', function(e) {
+            viewElement.val('preview');
+            viewElement.trigger('change');
+            // Set active class on the last chunk with user interaction.
+            setActiveChunk($(element));
+            // Remove active state on button.
+            removeActiveState(this);
+          });
+
+          // Switch to removed view when "Remove" button is pressed.
           $(classPrepend + 'remove-button', element).bind('mousedown.chunkEdit', function(e) {
             viewElement.val('removed');
             viewElement.trigger('change');
-            $(element).parents('#' + fieldName + '-' + delta + '-chunk-row').hide();
+            $('#' + fieldName + '-' + delta + '-chunk-row').hide();
+            // @TODO: reset odd/even classes.
+          });
+
+          // Switch to type_selection view for staged chunk when add after
+          // button is pressed.
+          $(classPrepend + 'add-after-button', element).bind('mousedown.chunkAdd', function(e) {
+            showStagedChunk('#' + fieldName + '-' + delta + '-chunk-row');
           });
 
           // Always set focus to the active chunk's button.
           if (active) {
             addButton.focus();
           }
+        });
+
+        // Switch to type_selection view for staged chunk when add before
+        // button is pressed.
+        $(':input[name="' + fieldName + '-add-before"]', context).bind('mousedown.chunkAdd', function(e) {
+          showStagedChunk('#' + fieldName + '-chunks-field .add-chunk-action-before-row');
         });
 
       });
