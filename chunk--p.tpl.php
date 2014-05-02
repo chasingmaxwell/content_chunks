3fd833d9273a3ec6c7232ca8042e0d063ec210c5
Chris Ruppel <chris.ruppel@gmail.com>
2014-05-02 08:29:30 -0500

    CSUMB-177: put newline sanitization inline.

--- chunk--p.tpl.php
+++ chunk--p.tpl.php
@@ -9,10 +9,8 @@
  * @TODO: add documentation about variables.
  */
 
-$configuration['p'] = trim(preg_replace('/\s+/', ' ', $configuration['p']));
-
 ?>
 
 <p class="<?php print $classes; ?>">
-  <?php print check_plain($configuration['p']); ?>
+  <?php print check_plain(trim(preg_replace('/\s+/', ' ', $configuration['p']))); ?>
 </p>
