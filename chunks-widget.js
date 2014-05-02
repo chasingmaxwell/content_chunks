28b7a2f167089d25760cccf38243116259dfd154
Peter Sieg <chasingmaxwell@gmail.com>
2014-05-02 18:58:05 -0400

    Make it easy to navigate and perform actions on the chunks field without moving your hands from the keyboard.

--- chunks-widget.js
+++ chunks-widget.js
@@ -7,8 +7,10 @@
 
   'use strict';
 
+  var removeActiveState, newChunkIndex;
+
   // Remove active state from element by briefly removing it from the DOM.
-  var removeActiveState = function(el) {
+  removeActiveState = function(el) {
     var p, s;
     p = el.parentNode;
     s = el.nextSibling;
@@ -41,7 +43,7 @@
           var stagedRow, stagedChunk, stagedDelta, stagedViewElement;
           stagedRow = $('tr.staged', field);
           stagedChunk = $('.chunk-wrapper', stagedRow);
-          stagedDelta = stagedChunk.attr('delta');
+          stagedDelta = parseInt(stagedChunk.attr('delta'), 10);
           stagedViewElement = $(':input[name="' + fieldName + '[' + langcode + '][' + stagedDelta + '][view]"]');
           stagedViewElement.val('type_selection');
           stagedViewElement.trigger('change');
@@ -58,7 +60,7 @@
           visibleChunks.each(function(i, element) {
             var delta, parentRow;
 
-            delta = $(element).attr('delta');
+            delta = parseInt($(element).attr('delta'), 10);
             parentRow = $('#' + fieldName + '-' + delta + '-chunk-row');
 
             if (((i + 1) % 2) == 1) {
@@ -90,10 +92,11 @@
           delete config[delta];
         };
 
+        // Perform actions on each chunk.
         chunks.each(function(index, element) {
           var delta, classPrepend, viewElement, view, active, addButton;
 
-          delta = $(element).attr('delta');
+          delta = parseInt($(element).attr('delta'), 10);
           viewElement = $(':input[name="' + fieldName + '[' + langcode + '][' + delta + '][view]"]');
           view = viewElement.val();
           classPrepend = '.' + classFieldName + '-' + delta + '-';
@@ -105,10 +108,13 @@
             $(element).parents('#' + fieldName + '-' + delta + '-chunk-row').hide();
           }
 
-          // Switch to configuration view when errors are detected.
-          if ($(classPrepend + 'configuration .error', element).length > 0) {
-            viewElement.val('configuration');
-            viewElement.trigger('change');
+          // If this chunk was just added, focus the type selection form
+          // element.
+          if (delta === newChunkIndex) {
+            // Set focus.
+            $(':input[name="' + fieldName + '[' + langcode + '][' + delta + '][type]"]', element).first().focus();
+            // Reset index until we add another new chunk.
+            newChunkIndex = undefined;
           }
 
           // Switch to configuration view upon type selection.
@@ -120,7 +126,7 @@
               viewElement.trigger('change');
               // Set active class on the last chunk with user interaction.
               setActiveChunk($(element));
-              // Add focus to first form item.
+              // Add focus to first configuration item.
               $('[name^="' + fieldName + '[' + langcode + '][' + delta + '][configuration]"]').first().focus();
             }
           });
@@ -132,7 +138,6 @@
               viewElement.trigger('change');
               // Set active class on the last chunk with user interaction.
               setActiveChunk($(element));
-              addButton.focus();
             }
           });
 
@@ -147,6 +152,8 @@
               removeActiveState(this);
               // Save configuration so we can restore it if we cancel.
               saveConfig(delta);
+              // Add focus to first configuration item.
+              $('[name^="' + fieldName + '[' + langcode + '][' + delta + '][configuration]"]').first().focus();
             }
           });
 
@@ -161,13 +168,29 @@
               removeActiveState(this);
               // Restore configuration.
               restoreConfig(delta);
+              // Set focus to add chunk button.
+              addButton.focus();
             }
           });
 
           // Switch to removed view when "Remove" button is pressed.
           $(classPrepend + 'remove-button', element).bind('keyup.chunkRemove mousedown.chunkRemove', function(e) {
             if (e.type === 'mousedown' || e.type === 'keyup' && e.keyCode === 13) {
-              e.preventDefault();
+              var visibleChunks = $('.chunk-wrapper:visible', field);
+              // Set focus to the add after button in the chunk before this one unless
+              // this is the first visible chunk in which case we should set the
+              // focus to the add before button.
+              visibleChunks.each(function(vi, ve) {
+                if ($(ve).attr('delta') === $(element).attr('delta')) {
+                  if (vi === 0) {
+                    $(':input[name="' + fieldName + '-add-before"]', field).focus();
+                  }
+                  else {
+                    $(visibleChunks[vi - 1]).find('.add-chunk-action-after input').focus();
+                  }
+                  return false;
+                }
+              });
               viewElement.val('removed');
               viewElement.trigger('change');
               $('#' + fieldName + '-' + delta + '-chunk-row').hide();
@@ -175,16 +198,39 @@
             }
           });
 
+          // The edit, cancel, and remove buttons should never reload the page
+          // if javascript is enabled.
+          $(classPrepend + 'edit-button, ' + classPrepend + 'cancel-button, ' + classPrepend + 'remove-button', element).click(function(e) {
+            e.preventDefault();
+          });
+
           // Switch to type_selection view for staged chunk when add after
           // button is pressed.
           $(classPrepend + 'add-after-button', element).bind('keyup.chunkAdd mousedown.chunkAdd', function(e) {
             if (e.type === 'mousedown' || e.type === 'keyup' && e.keyCode === 13) {
               showStagedChunk('#' + fieldName + '-' + delta + '-chunk-row');
+              // Iterate over visible chunks to find the new chunk index. We
+              // cannot rely on deltas since they can change when the field is
+              // reloaded (for instance, if there are chunks queued for removal
+              // above this current chunk).
+              $('.chunk-wrapper:visible', field).each(function(vi, ve) {
+                if ($(ve).attr('delta') === $(element).attr('delta')) {
+                  newChunkIndex = vi + 1;
+                  return false;
+                }
+              });
             }
           });
 
-          // Always set focus to the active chunk's button.
-          if (active) {
+          // Switch to configuration view when errors are detected.
+          if ($(classPrepend + 'configuration .error', element).length > 0) {
+            viewElement.val('configuration');
+            viewElement.trigger('change');
+            $(':input.error', element).first().focus();
+          }
+          // If no errors were detected, set the focus to the active chunk's add
+          // button.
+          else if (active) {
             addButton.focus();
           }
 
@@ -195,6 +241,7 @@
         $(':input[name="' + fieldName + '-add-before"]', field).bind('keyup.chunkAdd mousedown.chunkAdd', function(e) {
           if (e.type === 'mousedown' || e.type === 'keyup' && e.keyCode === 13) {
             showStagedChunk('#' + fieldName + '-chunks-field .add-chunk-action-before-row');
+            newChunkIndex = 0;
           }
         });
 
