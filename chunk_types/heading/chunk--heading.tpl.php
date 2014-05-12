c0045c1af297cb6de93d07aa838fa9e8b47db39c
Peter Sieg <chasingmaxwell@gmail.com>
2014-05-12 17:32:33 -0400

    [CSUMB-224] Add chunk type settings hook so that chunk types can be configured per field instance. Add entity and entity_type variables to chunk theme implementation. Remove image chunk type as it will be added back in as a submodule.

--- chunk_types/heading/chunk--heading.tpl.php
+++ chunk_types/heading/chunk--heading.tpl.php
@@ -12,6 +12,10 @@
  *   -  text: A string of text with which to fill the heading.
  *   -  level: An integer representing the header level (2, 3, 4, 5, or 6).
  * - $chunk_type: The ChunkType object representing the chunk's type.
+ * - $entity: If we are viewing an entity, this will be the entity object.
+ *   Otherwise it will be FALSE.
+ * - $entity_type: If we are viewing an entity, this will be the string
+ *   represnting the entity's type. Otherwise it will be FALSE.
  *
  * @see template_preprocess()
  * @see template_preprocess_chunk()
