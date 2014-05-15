eb8306e2d9a84c0d970bf822b7017c909476fb26
Peter Sieg <chasingmaxwell@gmail.com>
2014-05-15 11:52:51 -0400

    Add a client-side theme implementation for heading chunks.

---
+++ chunks_heading/chunks-heading.js
@@ -0,0 +1,17 @@
+/**
+ * @file Provide a client-side theme implementation for heading chunks.
+ */
+
+(function() {
+  Drupal.theme.prototype.chunk__heading = function(configuration) {
+    var output = '';
+
+    output += '<h' + configuration.level + ' class="chunk chunk-heading"';
+    if (configuration.id !== '') {
+      output += ' id="' + Drupal.checkPlain(configuration.id) + '"';
+    }
+    output += '>' + Drupal.checkPlain(configuration.text) + '</h' + configuration.level + '>';
+
+    return output;
+  };
+})();
