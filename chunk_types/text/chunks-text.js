3270e9b97134d97fa744f4226940c5bdcea81461
Peter Sieg <chasingmaxwell@gmail.com>
2014-05-15 16:52:13 -0400

    Provide @file in chunks-text.js because coding standards.

--- chunk_types/text/chunks-text.js
+++ chunk_types/text/chunks-text.js
@@ -1,3 +1,7 @@
+/**
+ * @file Provide a client-side theme implementation for text chunks.
+ */
+
 (function() {
   Drupal.theme.prototype.chunk__text = function(config) {
     var output = '<div class="chunk text-chunk">';
