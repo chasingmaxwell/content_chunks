77bed9667dd002131b179cfb13c925d513c60477
Peter Sieg <chasingmaxwell@gmail.com>
2014-05-17 00:54:54 -0400

    [CSUMB-238] Add a chunk type to create unordered and ordered lists with the chunks_list module.

---
+++ chunks_list/chunks-list.js
@@ -0,0 +1,12 @@
+/**
+ * @file Provide a client-side theme implementation for list chunks.
+ */
+
+(function() {
+  Drupal.theme.prototype.chunk__list = function(configuration) {
+    var output = '';
+
+
+    return output;
+  };
+})();
