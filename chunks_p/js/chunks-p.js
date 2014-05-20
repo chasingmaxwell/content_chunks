b23a539c3e1b618f936f1bd30174bd920f42d617
Chris Ruppel <chris@fourkitchens.com>
2014-05-20 07:57:40 -0500

    [CSUMB-177] Move Pen editor code into callback.

--- chunks_p/js/chunks-p.js
+++ chunks_p/js/chunks-p.js
@@ -2,11 +2,27 @@
  * @file Provide a client-side theme implementation for text chunks.
  */
 
-(function() {
+(function($) {
   Drupal.theme.prototype.chunk__p = function(config) {
     var output = '<div class="chunk p-chunk">';
     output += Drupal.checkPlain(config.p);
     output += '</div>';
     return output;
   };
-})();
+  Drupal.theme.prototype.chunk_callback__p = function(config, classPrepend) {
+    $(classPrepend + 'preview .p-chunk').each(function() {
+      // Initialize editor for this Paragraph chunk
+      var editor,
+          editorConfig;
+
+      editorConfig = {
+        class: 'pen-editor',
+        debug: true,
+        editor: this,
+        textarea: '<textarea name="content"></textarea>', // fallback for old browsers
+        list: ['bold', 'italic', 'underline', 'createlink']
+      };
+      editor = new Pen(editorConfig);
+    });
+  };
+})(jQuery);
