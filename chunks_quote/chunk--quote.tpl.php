15844f11b69b0f90cbdc53958da1bb2afbee031c
Peter Sieg <chasingmaxwell@gmail.com>
2014-06-16 18:15:12 -0500

    [CSUMB-252] A start towards multiple chunk type instances per field instance. Still needs some work.

--- chunks_quote/chunk--quote.tpl.php
+++ chunks_quote/chunk--quote.tpl.php
@@ -19,7 +19,8 @@
  * - $cite_attribute: A string containing the cite="" attribute or an empty
  *   string depending on whether the attribution_location configuration value is
  *   empty.
- * - $type_settings: An associative array representing the chunk type settings
+ * - $instance_settings: An associative array representing the chunk instance
+ *   settings
  *   connected to the chunk field's instance.
  * - $chunk_type: The ChunkType object representing the chunk's type.
  * - $entity: If we are viewing an entity, this will be the entity object.
