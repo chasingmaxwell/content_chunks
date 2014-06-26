78dfb1ffad44bca1441d6ae5d24f458b369b72c7
Peter Sieg <chasingmaxwell@gmail.com>
2014-06-26 17:26:17 -0400

    [CSUMB-252] Move all chunks submodules to a "modules" directory because it is tidy.

--- chunks_html/chunks-html.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file Provide a client-side theme implementation for full HTML chunks.
- */
-
-(function() {
-  Drupal.theme.prototype.chunk__html = function(configuration) {
-    return '<div class="chunk html-chunk">' + configuration.html + '</div>';
-  };
-})();
