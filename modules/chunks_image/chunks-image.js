7c4fb2be97744de24a3abd3111cc26b0a82e7a44
Peter Sieg <chasingmaxwell@gmail.com>
2014-07-09 11:47:46 -0400

    [CSUMB-262] Correctly detect whether or not we should link to content in an image chunk preview. Focus image style selection after upload.

--- modules/chunks_image/chunks-image.js
+++ modules/chunks_image/chunks-image.js
@@ -3,19 +3,37 @@
  */
 
 (function($) {
-  Drupal.behaviors.chunksImageRemove = {
+  Drupal.behaviors.chunksImageCallbacks = {
     attach: function(context, settings) {
-      // Hide the cancel button if you remove an image. If the user were to
-      // press cancel it would seem as if their previously uploaded image would
-      // remain in the chunk untouched. This is not the case, however, since
-      // that file was actually removed from the files directory.
-      $('.image-widget .form-submit[value="' + Drupal.t('Remove') + '"]', context).bind('mousedown.removeChunkImage', function(e) {
-        // Only do this if we are operating on an image chunk. Other chunk types
-        // may provide an image widget.
-        if ($(this).parents('.image-chunk-type-configuration').length > 0) {
-          $(this).parents('.chunk-wrapper').find('.chunk-cancel-button').hide();
-        }
-      });
+
+      if (typeof Drupal.settings.chunks.callbacks.initialize.image === 'undefined') {
+        // Implements the initialize callback to perform actions on a image
+        // Chunk object upon initialization.
+        Drupal.settings.chunks.callbacks.initialize.image = function(chunk) {
+
+          // Only perform certain actions if the current chunk is not assigned
+          // to a different type.
+          if (chunk.chunkType === '' || chunk.chunkType === 'image') {
+
+            // Hide the cancel button if you remove an image. If the user were to
+            // press cancel it would seem as if their previously uploaded image would
+            // remain in the chunk untouched. This is not the case, however, since
+            // that file was actually removed from the files directory.
+            $('.image-widget .form-submit[value="' + Drupal.t('Remove') + '"]', chunk.element).bind('mousedown.removeChunkImage', function(e) {
+              if (e.which === 1) {
+                chunk.element.find('.chunk-cancel-button').hide();
+              }
+            });
+
+            // Set focus to image style selection after pressing "Upload" button for
+            // better usability.
+            $('.image-widget .form-submit[value="' + Drupal.t('Upload') + '"]', context).bind('mousedown.removeChunkImage', function(e) {
+              chunk.element.find('[name="' + chunk.namePrepend + '[configuration][image][image_style]"]').focus();
+            });
+          }
+        };
+      }
+
     }
   };
 })(jQuery);
