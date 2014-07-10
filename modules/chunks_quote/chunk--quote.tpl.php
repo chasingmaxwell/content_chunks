50cd3cd6162f3cc0ff8b4a5f1f2efb221cf90f4e
Peter Sieg <chasingmaxwell@gmail.com>
2014-07-10 12:07:59 -0400

    [CSUMB-248] Make sure edit_in_place always exists in the configuration for edit_in_place chunks so we do not need to use isset in templates.

--- modules/chunks_quote/chunk--quote.tpl.php
+++ modules/chunks_quote/chunk--quote.tpl.php
@@ -42,10 +42,10 @@
 ?>
 
 <blockquote class="<?php print $classes; ?>"<?php print $cite_attribute; ?>>
-  <p<?php if (isset($configuration['edit_in_place'])): ?> contenteditable<?php endif; ?>><?php print filter_xss(trim(preg_replace('/(\s|<br>|<\/?div>)+/', ' ', $configuration['quote'])), array('a', 'b', 'em', 'i', 'strong', 'u')); ?></p>
-  <?php if (!empty($configuration['attribution']) || isset($configuration['edit_in_place'])): ?>
+  <p<?php if ($configuration['edit_in_place']): ?> contenteditable<?php endif; ?>><?php print filter_xss(trim(preg_replace('/(\s|<br>|<\/?div>)+/', ' ', $configuration['quote'])), array('a', 'b', 'em', 'i', 'strong', 'u')); ?></p>
+  <?php if (!empty($configuration['attribution']) || $configuration['edit_in_place']): ?>
     <footer>
-      <cite<?php if (isset($configuration['edit_in_place'])): ?> contenteditable<?php endif; ?>><?php print filter_xss(trim(preg_replace('/(\s|<br>|<\/?div>)+/', ' ', $configuration['attribution'])), array('a', 'b', 'em', 'i', 'strong', 'u')); ?></cite>
+      <cite<?php if ($configuration['edit_in_place']): ?> contenteditable<?php endif; ?>><?php print filter_xss(trim(preg_replace('/(\s|<br>|<\/?div>)+/', ' ', $configuration['attribution'])), array('a', 'b', 'em', 'i', 'strong', 'u')); ?></cite>
     </footer>
   <?php endif; ?>
 </blockquote>
